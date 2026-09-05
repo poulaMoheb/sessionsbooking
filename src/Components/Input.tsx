79637b517768c4fac79deb364d42fe3e417d6860
paula moheb <poulamoheb12@gmail.com>
2026-09-05 19:51:36 +0300

    add Input component for user input handling

---
+++ src/Components/Input.tsx
@@ -0,0 +1,20 @@
+import { forwardRef, type ComponentPropsWithoutRef } from "react"
+
+type InputProps = {
+    id: string,
+    label: string
+} & ComponentPropsWithoutRef<'input'>;
+
+
+
+
+function Input({ id, label, ...props }: InputProps) {
+    return (
+        <div>
+            <label htmlFor={id}>{label}</label>
+            <input id={id} {...props}></input>
+        </div>
+    )
+}
+
+export default Input
